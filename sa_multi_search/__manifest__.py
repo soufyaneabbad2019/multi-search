fb78a0ab41afddf86b5a28066d6f7339b5178c4d
SoufyaneAbbad <soufiane.abbad2019@gmail.com>
2026-09-01 07:46:43 +0000

    [UPD] sa_multi_search: English-only description, larger use case screenshots, price 40 -> 25 EUR

--- sa_multi_search/__manifest__.py
+++ sa_multi_search/__manifest__.py
@@ -17,7 +17,7 @@ Works on any Odoo view: Products, Sales Orders, Purchase Orders, Inventory, Manu
     'author': 'Soufyane Abbad',
     'website': 'https://www.linkedin.com/in/soufyane-abbad',
     'license': 'OPL-1',
-    'price': 40.0,
+    'price': 25.0,
     'currency': 'EUR',
     'depends': ['web'],
     'assets': {
